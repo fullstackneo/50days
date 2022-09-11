08c29a08a21b86ed941bbe8fcca1e725037a1ae5
Neo <fullstackneo@gmail.com>
2022-09-11 17:08:23 -0600

    optomize front end

--- background-slider/src/component/slider.js
+++ background-slider/src/component/slider.js
@@ -32,10 +32,12 @@ let sliderComponent = /*html*/ ` <section
     <div class="current relative left-[-10vw] top-[-10vh] h-screen w-screen bg-cover" style="background-image:url(${sliders[0].url})"></div>
     <span
       class="left fixed top-1/2 h-16 w-16 -translate-y-1/2 -translate-x-full cursor-pointer border-[2px] border-r-0 border-solid border-[#ffa500] text-center font-[FontAwesome] text-2xl leading-[60px] text-white"
+      onmouseover="loadImg(-1)"
       ></span
     >
     <span
       class="right fixed top-1/2 left-[90vw] h-16 w-16 -translate-y-1/2 cursor-pointer border-[2px] border-l-0 border-solid border-[#ffa500] text-center font-[FontAwesome] text-2xl leading-[60px] text-white"
+      onmouseover="loadImg(1)"
       ></span
     >
   </div>
@@ -47,20 +49,44 @@ document.querySelector('body').innerHTML = sliderComponent;
 let sliderEl = document.querySelector('.slider');
 let currentSliderEl = document.querySelector('.current');
 let imgs = sliderEl.querySelectorAll('img');
+let num = +sliderEl.dataset.cur;
+let sliderCache = new Set('0');
 
+// preload next img
+let loadImg = direction => {
+  let img = new Image();
+  let newNum;
+  if (direction < 0) {
+    newNum = num === 0 ? sliders.length - 1 : num - 1;
+    if (sliderCache.has(newNum)) return;
+  } else {
+    newNum = num === sliders.length - 1 ? 0 : num + 1;
+    if (sliderCache.has(newNum)) return;
+  }
+  img.src = sliders[newNum].url;
+  sliderCache.add(newNum);
+};
+
+// click left or right
 sliderEl.addEventListener('click', e => {
   if (!e.target.classList.contains('left') && !e.target.classList.contains('right')) return;
 
-  let num = +sliderEl.dataset.cur;
   // update current slider num
   if (e.target.classList.contains('left')) {
     num = num - 1 < 0 ? 4 : num - 1;
     sliderEl.dataset.cur = num;
+    // update bgImg of element
+    sliderEl.style.backgroundImage = `url(${sliders[num].url})`;
+    currentSliderEl.style.backgroundImage = `url(${sliders[num].url})`;
+    //load next img
+    loadImg(-1);
   } else {
     num = num + 1 === 5 ? 0 : num + 1;
     sliderEl.dataset.cur = num;
+    // update bgImg of element
+    sliderEl.style.backgroundImage = `url(${sliders[num].url})`;
+    currentSliderEl.style.backgroundImage = `url(${sliders[num].url})`;
+    //load next img
+    loadImg(1);
   }
-  // update bgImg of element
-  sliderEl.style.backgroundImage = `url(${sliders[num].url})`;
-  currentSliderEl.style.backgroundImage = `url(${sliders[num].url})`;
 });
