788aab4cb5764b53fbf71b17d14dd85a98d0684d
Neo <fullstackneo@gmail.com>
2022-09-25 22:18:25 -0600

    update data

--- testimonial-box/src/components/testimonial-box/data.js
+++ testimonial-box/src/components/testimonial-box/data.js
@@ -2,22 +2,22 @@ export let data = [
   {
     intro:
       'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
-    name: 'Neo1',
+    name: 'Neo Ji',
     avatar: 'https://static.remove.bg/remove-bg-web/404892c8db150794c87943e5ecc332f781da0122/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
     title: 'Software Engineer',
   },
   {
     intro:
-      'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
-    name: 'Neo2',
+      "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
+    name: 'Jonathan Nunfiez',
     avatar: 'https://static.remove.bg/remove-bg-web/404892c8db150794c87943e5ecc332f781da0122/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
-    title: 'Software Engineer',
+    title: 'Accountant',
   },
   {
     intro:
-      'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
-    name: 'Neo3',
+      'This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
+    name: 'Sasha Ho',
     avatar: 'https://static.remove.bg/remove-bg-web/404892c8db150794c87943e5ecc332f781da0122/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
-    title: 'Software Engineer',
+    title: 'Director',
   },
 ];
