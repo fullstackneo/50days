4cc79faa4d7c0a225ae5a3b793fc67b283097354
Neo <fullstackneo@gmail.com>
2022-09-25 23:02:02 -0600

    image feed

---
+++ image-feed/src/pages/Home/index.js
@@ -0,0 +1,6 @@
+// render component to page
+import { ImageFeed } from './../../components/component/template.js';
+document.querySelector('#image-feed').innerHTML = ImageFeed;
+
+// insert DOM operation for component
+import('./../../components/component/dom.js');
