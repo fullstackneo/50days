7fca38c7199efcc06e9e77d13df0ce89eb8b1d65
Neo <fullstackneo@gmail.com>
2022-09-27 02:39:06 -0600

    refactor

--- catch-insect/src/components/component/dom.js
+++ catch-insect/src/components/component/dom.js
@@ -1,2 +1,99 @@
-import {} from './template.js';
-import {} from './data.js'
+import { InsectCard } from './template.js';
+let insects = [
+  { name: 'Fly', url: 'https://pngimg.com/uploads/fly/fly_PNG3946.png' },
+  { name: 'Mosquito', url: 'http://pngimg.com/uploads/mosquito/mosquito_PNG18175.png' },
+  { name: 'Spider', url: 'http://pngimg.com/uploads/spider/spider_PNG12.png' },
+  { name: 'Roach', url: 'http://pngimg.com/uploads/roach/roach_PNG12163.png' },
+];
+
+
+let catchInsect = document.querySelector('.catch-the-insect');
+let pageEl = catchInsect.querySelector('.page');
+
+// welcome section
+let startBtn = catchInsect.querySelector('#welcome button');
+startBtn.onclick = () => pageEl.classList.add('-translate-y-full');
+
+// chooice-insect section
+let insectsEl = catchInsect.querySelector('#choose-insect ul');
+insectsEl.innerHTML = insects.reduce((res, cur) => res + InsectCard(cur), '');
+let insect;
+insectsEl.onclick = e => {
+  if (e.target.tagName === 'LI') {
+    pageEl.classList.add('-translate-y-[200%]');
+    insect = e.target.closest('li').lastElementChild.src;
+    // create first insect
+    createInsect(insect);
+    // GC
+    startBtn = null;
+  }
+};
+
+// playground section
+let gameWidth = catchInsect.offsetWidth;
+let gameHeight = catchInsect.offsetHeight;
+let playgroundEl = catchInsect.querySelector('#playground');
+let scoreEl = catchInsect.querySelector('.score span');
+let randomDegree = () => parseInt(Math.random() * 360);
+let randomPosition = length => parseInt(Math.random() * (length - 210) + 105);
+
+let score = 0;
+let createInsect = time => {
+  setTimeout(() => {
+    playgroundEl.insertAdjacentHTML(
+      'beforeend',
+      /*html*/ `<img class="insect absolute h-[110px] object-contain duration-1000 cursor-pointer" src="${insect}" alt="fly" style="left:${randomPosition(gameWidth)}px;top:${randomPosition(gameHeight)}px;transform:scale(1,1) rotate(${randomDegree()}deg);"/>`
+    );
+  }, time);
+};
+
+// set timer
+let hrNum = playgroundEl.querySelector('.timer .hr');
+let minNum = playgroundEl.querySelector('.timer .min');
+let gameTimer;
+const startTimer = () => {
+  let hr = 0;
+  let min = 0;
+  gameTimer = setInterval(() => {
+    if (min === 59) {
+      hrNum.textContent = hr + 1 < 10 ? '0' + (hr + 1) : hr + 1;
+      minNum.textContent = '00';
+      min = 0;
+      hr++;
+    } else {
+      minNum.textContent = min + 1 <= 9 ? '0' + (min + 1) : min + 1;
+    }
+    min++;
+  }, 1000);
+};
+
+startTimer();
+
+
+let gameOver = () => {
+  playgroundEl.insertAdjacentHTML(
+    'beforeend',
+    /*html*/ `<div class="absolute top-1/4 flex h-[100px] w-full flex-col items-center justify-center bg-[#00000065] text-center z-10">
+      <p class='leading-[30px]'>Are you annoyed yet?<br />You are playing an impossible game!!</p>
+    </div>`
+  );
+};
+
+// click insect and create more
+playgroundEl.onclick = e => {
+  if (e.target.classList.contains('insect')) {
+    e.target.style.transform = e.target.style.transform.replace('scale(1, 1)', 'scale(0,0)');
+
+    scoreEl.textContent = ++score;
+    setTimeout(() => {
+      e.target.remove();
+      createInsect(750);
+      createInsect(1500);
+    }, 500);
+
+    if (score === 20) {
+      clearInterval(gameTimer);
+      gameOver();
+    }
+  }
+};
--- catch-insect/src/components/component/template.js
+++ catch-insect/src/components/component/template.js
@@ -1,1 +1,22 @@
-// export const component=/*html*/``
+export const CatchInsect = /*html*/ `<div class="catch-the-insect h-full overflow-hidden font-press-start-2p text-white">
+        <div class="page h-full duration-400 children:h-full">
+          <div id="welcome" class="flex flex-col items-center justify-center bg-primary">
+            <h2 class="mb-5 text-center text-[40px] font-bold">Catch The Insect</h2>
+            <button class="bg-white px-7 py-4 text-base text-primary hover:bg-[#edf0ff]">Play Game</button>
+          </div>
+          <div id="choose-insect" class="flex flex-col items-center justify-center bg-primary">
+            <h2 class="mb-10 text-center text-[40px] font-bold">What is your "favorite" insect?</h2>
+            <ul class="flex items-center justify-center children:mx-3">
+            </ul>
+          </div>
+          <div id="playground" class="relative bg-primary children:select-none">
+            <div class="timer absolute left-5 top-10 text-2xl font-bold">Time: <span class="hr">00</span>:<span class="min">00</span></div>
+            <div class="score absolute top-10 right-5 text-2xl font-bold">Score: <span>0</span></div>
+          </div>
+        </div>
+      </div>`;
+
+export const InsectCard = ({ name, url }) => /*html*/ `<li class="flex h-[185px] w-[185px] cursor-pointer flex-col items-center justify-center border-2 hover:bg-white hover:text-primary children:pointer-events-none">
+  <p class="mb-1">${name}</p>
+  <img class="h-[100px] object-contain" src="${url}" alt="${name}" />
+</li>`;
