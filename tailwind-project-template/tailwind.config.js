664ab104e58ca9d55f7d30cf7b45b7931240573a
Neo <fullstackneo@gmail.com>
2022-09-27 03:50:44 -0600

    update template

--- tailwind-project-template/tailwind.config.js
+++ tailwind-project-template/tailwind.config.js
@@ -3,8 +3,20 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   darkMode: 'class',
   plugins: [
-    function ({ addVariant }) {
+    function ({ addVariant, addUtilities }) {
       addVariant('children', '& > *');
+      addUtilities({
+        '.rotate-x-40': {
+          // transform will cover el.style.transform
+          transform: 'rotateX(40deg)',
+        },
+        '.rotate-y-40': {
+          transform: 'rotateY(40deg)',
+        },
+        '.animation-delay-100': {
+          'animation-delay': '100ms',
+        },
+      });
     },
   ],
   theme: {
@@ -12,22 +24,7 @@ module.exports = {
       fontFamily: {
         'fa': 'FontAwesome',
         'roboto': 'Roboto,"sans-serif"',
-        sans: [
-          'Arial',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          '"Segoe UI"',
-          'Roboto',
-          '"Helvetica Neue"',
-          '"Noto Sans"',
-          'sans-serif',
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
+        sans: ['Arial', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
         serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
         mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
       },
@@ -55,7 +52,7 @@ module.exports = {
         800: '800ms',
         900: '900ms',
       },
-            borderWidth: {
+      borderWidth: {
         1: '1px',
         3: '3px',
         5: '5px',
